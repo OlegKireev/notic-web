65d3f6f9b0423eda67cddd047aef2f81502a6168
Oleg Kireev <okireev@ubic.tech>
2022-01-12 11:36:17 +0300

    @/index: wrap the App with Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
+const WrappedApp = () => (
+  <Router>
+    <App />
+  </Router>
+)
+
 ReactDOM.render(
-  <App />,
+  <WrappedApp />,
   document.getElementById('root')
 );
