30ef1ef136dc207f359b21645c68b86c6b02ad40
Oleg Kireev <okireev@ubic.tech>
2022-01-12 13:49:30 +0300

    @/components/Header: add styled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from '@/assets/img/logo.svg';
-import Navigation from '../Navigation';
+import { HeaderBar, Logo, LogoText } from './styled';
 
 const Header = () => (
-  <header>
-    <img src={logo} alt="Notedly Logo" height="40" />
-    <h1>Notedly</h1>
-  </header>
+  <HeaderBar>
+    <Logo src={logo} alt="Notedly Logo" height="40" />
+    <LogoText>Notedly</LogoText>
+  </HeaderBar>
 );
 
 export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { widths } from "@/constants/breakpoints";
+
+export const HeaderBar = styled.header`
+  width: 100%;
+  padding: 0.5em 1em;
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  border-bottom: 1px solid #e7e8ec;
+  z-index: 1;
+`;
+
+export const LogoText = styled.h1`
+  margin: 0;
+  padding: 0;
+  display: inline;
+  margin-left: .5em;
+  font-size: 1.5rem;
+
+  @media screen and (max-width: ${widths.md}) {
+    font-size: 1.2rem
+  }
+`;
+
+export const Logo = styled.img`
+  @media screen and (max-width: ${widths.md}) {
+    height: 20px;
+  }
+`;
