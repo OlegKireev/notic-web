fc3efa9119bea2388dc863873e7cd5e1007bea60
Oleg Kireev <okireev@ubic.tech>
2022-01-12 16:43:14 +0300

    @/components/Layout: calculate Wrapper height

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import logo from '@/assets/img/logo.svg';
 import { HeaderBar, Logo, LogoText, LogoLink } from './styled';
 
-const Header = () => (
-  <HeaderBar>
+const Header = forwardRef((props, ref) => (
+  <HeaderBar {...props} ref={ref}>
     <LogoLink to="/">
       <Logo src={logo} alt="Notedly Logo" height="40" />
       <LogoText>Notedly</LogoText>
     </LogoLink>
   </HeaderBar>
-);
+));
 
 export default Header;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Header from '../Header';
 import Navigation from '../Navigation';
 import { Container, Wrapper, Aside, Main } from './styled';
 
 const Layout = ({
   children
-}) => (
-  <Container>
-    <Header />
-    <Wrapper>
-      <Aside>
-        <Navigation />
-      </Aside>
-      <Main>
-        {children}
-      </Main>
-    </Wrapper>
-  </Container>
-)
+}) => {
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    if (!headerRef.current) {
+      return
+    }
+    setHeaderHeight(headerRef.current.clientHeight);
+  }, [headerRef])
+
+  return (
+    <Container>
+      <Header ref={headerRef} />
+      <Wrapper style={{
+        height: `calc(100% - ${headerHeight}px)`
+      }}>
+        <Aside>
+          <Navigation />
+        </Aside>
+        <Main>
+          {children}
+        </Main>
+      </Wrapper>
+    </Container>
+  )
+}
 
 export default Layout;
