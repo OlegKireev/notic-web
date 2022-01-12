5af5d6faedbae9f66da2f02815a7b53110a9a6a5
Oleg Kireev <okireev@ubic.tech>
2022-01-12 16:59:06 +0300

    @/App: connect with Apollo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Pages from './pages';
 import GlobalStyle from './theme/GlobalStyle';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './api';
 
 const App = () => {
   return (
-    <Fragment>
+    <ApolloProvider client={apolloClient}>
       <GlobalStyle />
       <Pages />
-    </Fragment>
+    </ApolloProvider>
   )
 };
 
