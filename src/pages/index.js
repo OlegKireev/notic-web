713d2ef0be65aa0dfd6b357b87c4e0200efce962
Oleg Kireev <okireev@ubic.tech>
2022-01-12 11:36:55 +0300

    @/pages: remove Router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './home';
 import MyNotes from './my-notes';
 import Favorites from './favorites';
 
 const Pages = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/my-notes" component={MyNotes} />
-        <Route path="/favorites" component={Favorites} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/my-notes" component={MyNotes} />
+      <Route path="/favorites" component={Favorites} />
+    </Switch>
   )
 }
 
