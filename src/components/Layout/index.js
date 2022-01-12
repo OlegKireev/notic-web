3d7544f8fadd45e272daacd59fbd273858ebdb62
Oleg Kireev <okireev@ubic.tech>
2022-01-12 13:51:32 +0300

    @/components/Layout: add styled

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Header from '../Header';
 import Navigation from '../Navigation';
+import { Container, Wrapper, Aside, Main } from './styled';
 
 const Layout = ({
   children
 }) => (
-  <div>
+  <Container>
     <Header />
-    <Navigation />
-    <main>
-      {children}
-    </main>
-  </div>
+    <Wrapper>
+      <Aside>
+        <Navigation />
+      </Aside>
+      <Main>
+        {children}
+      </Main>
+    </Wrapper>
+  </Container>
 )
 
 export default Layout;
---
+++ src/components/Layout/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { widths } from "@/constants/breakpoints";
+
+export const Container = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    height: 100%;
+
+    @media screen and (max-width: ${widths.md}) {
+      flex-direction: column;
+    }
+`;
+
+export const Aside = styled.aside`
+  padding: 1em;
+  background: #f5f4f0;
+  width: 15.75rem;
+  height: 100%;
+  flex-shrink: 0;
+
+  @media (max-width: ${widths.md}) {
+    height: auto;
+    width: auto;
+    padding: .5em 1em;
+  }
+`;
+
+export const Main = styled.main`
+  overflow-y: scroll;
+  padding: 1em;
+  min-height: 100%;
+  width: 100%;
+`;
