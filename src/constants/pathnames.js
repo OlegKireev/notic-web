e8b80c625590b3b719ee56870877a3ac28716423
Oleg Kireev <okireev@ubic.tech>
2022-01-12 11:34:40 +0300

    @/constants/pathnames: create

---
+++ src/constants/pathnames.js
@@ -0,0 +1,17 @@
+export const pathnames = [
+  {
+    path: '/',
+    label: 'Home',
+    exact: true,
+  },
+  {
+    path: '/my-notes',
+    label: 'My notes',
+  },
+  {
+    path: '/favorites',
+    label: 'Favotites',
+  },
+];
+
+export default pathnames;
