63db6a68df671850e2d0c458d4386f468beceab3
Oleg Kireev <okireev@ubic.tech>
2022-01-12 19:15:33 +0300

    @/components/Note: add styled

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { format } from 'date-fns'
+import { Wrapper, Author, Avatar, MetaInfo } from './styled';
 
 const Note = ({ data }) => {
+  const formatedDate = format(data.createdAt, 'MMM Do YYYY');
+
   return (
-    <article>
-      <img src={data.author.avatar} alt={`${data.author.username} avatar`} height={25} />
-      <div>
+    <Wrapper>
+      <Author>
+        <Avatar src={data.author.avatar} alt={`${data.author.username} avatar`} height={25} />
         <span>{data.author.username}</span>{' '}
-        <span>{data.favoriteCount}</span>{' '}
-        <time dateTime={data.createdAt}>{data.createdAt}</time>
-      </div>
+      </Author>
       <ReactMarkdown source={data.content} />
-    </article>
+      <MetaInfo>
+        <span>Favorites: {data.favoriteCount}</span>
+        <time dateTime={data.createdAt}>{formatedDate}</time>
+      </MetaInfo>
+    </Wrapper>
   )
 };
 
---
+++ src/components/Note/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.article`
+  max-width: 800px;
+  margin: 0 auto;
+`
+
+export const Author = styled.div`
+  display: flex;
+`
+
+export const Avatar = styled.img`
+  max-width: 1.5rem;
+  max-height: 1.5rem;
+  margin-right: .5em;
+  object-fit: cover;
+`
+
+export const MetaInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
