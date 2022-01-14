1806823479f9cc9eba2157560f50cb3364515f0e
Oleg Kireev <okireev@ubic.tech>
2022-01-14 16:13:02 +0300

    @/components/NoteList: add LoadMore button

--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Preloader from '../Preloader';
 import Note from '../Note';
 import { List } from './styled';
+import Button from '../Button/styled';
 
-const NoteList = ({ data, loading }) => (
+const NoteList = ({ data, loading, hasMore, onLoadMoreClick }) => (
   <Preloader loading={loading}>
     <List>
       {data.map((note) => (
         <Note data={note} key={note.id} />
       ))}
     </List>
+    {hasMore && <Button onClick={() => onLoadMoreClick()}>Load more...</Button>}
   </Preloader>
 )
 
