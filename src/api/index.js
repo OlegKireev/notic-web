f8ab16f49bb0428f326a63a3816623172d36b884
Oleg Kireev <okireev@ubic.tech>
2022-01-14 17:56:20 +0300

    @/api/index: attach 'authorization' header to every query

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from 'apollo-link-context';
 
 const uri = process.env.API_URI;
 const cache = new InMemoryCache();
+const httpLink = createHttpLink({ uri });
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || '',
+    }
+  }
+})
 
 const apolloClient = new ApolloClient({
   uri,
+  link: authLink.concat(httpLink),
   cache,
   connectToDevTools: true,
 })
