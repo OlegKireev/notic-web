999288112b89f20a5719a53f98df104d804ddc91
Oleg Kireev <okireev@ubic.tech>
2022-01-14 18:48:53 +0300

    @/components/Header: add handleLogoutClick

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,26 @@ import React, { forwardRef } from 'react';
 import logo from '@/assets/img/logo.svg';
 import { HeaderBar, Logo, LogoText, LogoLink } from './styled';
 import { IS_LOGGED_IN } from '@/api/auth';
-import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
+import { useApolloClient, useQuery } from '@apollo/client';
+import { Link, useHistory } from 'react-router-dom';
+import Button from '../Button';
 
 const Header = forwardRef((props, ref) => {
+  const history = useHistory();
   const { data } = useQuery(IS_LOGGED_IN);
   const { isLoggedIn } = data; 
+  const client = useApolloClient();
+
+  const handleLogOutClick = () => {
+    // Удаляем токен
+    localStorage.removeItem('token');
+    // Очищаем кэш приложения
+    client.resetStore();
+    // Обновляем локальное состояние
+    client.writeData({ data: { isLoggedIn: false } });
+    // Перенаправляем пользователя на домашнюю страницу
+    history.push('/');
+  };
 
   return (
     <HeaderBar {...props} ref={ref}>
@@ -17,11 +31,14 @@ const Header = forwardRef((props, ref) => {
       </LogoLink>
       <div>
         {isLoggedIn
-          ? <Link>Log out</Link>
+          ? <Button 
+              kind="text"
+              onClick={handleLogOutClick}
+            >Log out</Button>
           : (
             <div>
-              <Link to="/sign in">Sign in</Link>
-              <Link to="/sign up">Sign up</Link>
+              <Link to="/sign-in">Sign in</Link>
+              <Link to="/sign-up">Sign up</Link>
             </div>
           )}
       </div>
