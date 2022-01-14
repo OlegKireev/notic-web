96165ff2813c5ec0e7274ac6db812f86b91cd1b0
Oleg Kireev <okireev@ubic.tech>
2022-01-14 16:53:43 +0300

    @/components/Button: throw props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import StyledButton from "./styled";
 
-const Button = () => (
-  <StyledButton />
+const Button = ({...props}) => (
+  <StyledButton {...props}/>
 )
 
 export default Button;
