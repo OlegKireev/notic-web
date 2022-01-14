566a42a2433705afbc9bf985401d438f2b472ce5
Oleg Kireev <okireev@ubic.tech>
2022-01-14 15:45:58 +0300

    @/pages/note: create

--- src/pages/note.js
+++ src/pages/note.js
@@ -1,10 +1,28 @@
 import React, { Fragment, useEffect } from 'react';
-// import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { GET_NOTE } from '@/api/note';
+import Preloader from '@/components/Preloader';
+import Note from '@/components/Note';
 
 const MyNotes = ({match}) => {
+  const id = match.params.id;
+
+  const { data, loading, error } = useQuery(GET_NOTE, { variables: { id }})
+  
+  useEffect(() => {
+    if (error) {
+      console.error(error.message);
+    }
+  }, [error])
+
   return (
     <Fragment>
-      <p>ID: {match.params.id}</p>
+      {
+        <Preloader loading={loading}>
+          {error && <p>Note not found.</p>}
+          {data && <Note data={data.note} />}
+        </Preloader>
+      }
     </Fragment>
   );
 };
