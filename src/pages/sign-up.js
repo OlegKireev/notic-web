13ad6aa407e714f90e48d8cf71034560f1e39316
Oleg Kireev <okireev@ubic.tech>
2022-01-14 17:28:24 +0300

    @/pages/sign-up: generate initial form state

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,12 +1,37 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import LoginForm from '@/components/LoginForm';
 
+const signUpControls = {
+  name: {
+    id: 'name',
+    label: 'Username:',
+    placeholder: 'jonh89',
+    required: true,
+  },
+  email: {
+    id: 'email',
+    label: 'E-mail',
+    placeholder: 'jonhmalcovich@gmail.com',
+    required: true,
+  },
+  password: {
+    id: 'password',
+    label: 'Password:',
+    required: true,
+    type: 'password',
+  }
+};
+
+const createFormEmptyState = (controls) => {
+  return Object
+  .keys(signUpControls)
+  .reduce((acc, key) => {
+    return { ...acc, [key]: ''};
+  }, {});
+};
+
 const SignUp = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
+  const [formData, setFormData] = useState(createFormEmptyState(signUpControls));
 
   useEffect(() => {
     document.title = 'Sign Up — Notedly';
@@ -27,6 +52,7 @@ const SignUp = () => {
     <Fragment>
       <LoginForm 
         data={formData}
+        controls={signUpControls}
         onInputsChange={handleFormInputChange}
         onSubmit={handleFormSubmit}  
       />
