3481537cefcedacdb127c3ed67c4873e9992ce3c
Oleg Kireev <okireev@ubic.tech>
2022-01-14 17:07:31 +0300

    @;pages/sign-up: make form state and handlers

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,13 +1,35 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import LoginForm from '@/components/LoginForm';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
   useEffect(() => {
     document.title = 'Sign Up — Notedly';
-  });
+  }, []);
+
+  const handleFormInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <Fragment>
-      <LoginForm />
+      <LoginForm 
+        data={formData}
+        onInputsChange={handleFormInputChange}
+        onSubmit={handleFormSubmit}  
+      />
     </Fragment>
   );
 };
