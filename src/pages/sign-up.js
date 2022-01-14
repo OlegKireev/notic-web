f0e5ca5c16101c55aa2a583df03355cbcdfd14c5
Oleg Kireev <okireev@ubic.tech>
2022-01-14 16:35:02 +0300

    @/pages/sign-up: add form

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
+import Button from '../components/Button/styled';
 
 const SignUp = () => {
   useEffect(() => {
@@ -6,7 +7,33 @@ const SignUp = () => {
   });
   return (
     <Fragment>
-      <p>Sign Up page</p>
+      <form>
+        <label htmlFor='username'>Username:</label>
+        <input
+          required
+          type="text"
+          id="username"
+          name="username"
+          placeholder="username"
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          required
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          required
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+        />
+        <Button type='submit'>Sign up</Button>
+      </form>
     </Fragment>
   );
 };
