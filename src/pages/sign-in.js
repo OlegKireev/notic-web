32976196820aa1e1b75b84c4a91b958ce9111c19
Oleg Kireev <okireev@ubic.tech>
2022-01-16 21:39:52 +0300

    @/pages/sign-in: create

---
+++ src/pages/sign-in.js
@@ -0,0 +1,69 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import UserForm from '@/components/UserForm';
+import useLogin from '../hooks/useLogin';
+
+const signInControls = {
+  login: {
+    id: 'login',
+    label: 'Login:',
+    placeholder: 'jonh89 or jonhmalcovich@gmail.com',
+  },
+  password: {
+    id: 'password',
+    label: 'Password:',
+    required: true,
+    type: 'password',
+  }
+};
+
+const createFormEmptyState = (controls) => {
+  return Object
+  .keys(controls)
+  .reduce((acc, key) => {
+    return { ...acc, [key]: ''};
+  }, {});
+};
+
+const SignIn = () => {
+  const { signIn, loading, error } = useLogin();
+
+  const [formData, setFormData] = useState(createFormEmptyState(signInControls));
+
+  useEffect(() => {
+    document.title = 'Sign In — Notedly';
+  }, []);
+
+  const handleFormInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const loginType = formData.login.indexOf('@') >= 0 ? 'email' : 'username';
+    console.log(loginType);
+    signIn({
+      variables: {
+        ...formData,
+        [loginType]: formData.login,
+      }
+    })
+  };
+
+  return (
+    <Fragment>
+      <UserForm 
+        data={formData}
+        controls={signInControls}
+        submitText="Login"
+        loading={loading}
+        error={error}
+        onInputsChange={handleFormInputChange}
+        onSubmit={handleFormSubmit}  
+      >Sign In:</UserForm>
+    </Fragment>
+  );
+};
+
+export default SignIn;
