f7b9e1239098b49fea220c8a7754a0ab7d3cf380
Oleg Kireev <okireev@ubic.tech>
2022-01-17 10:41:15 +0300

    @/api/cache: create

---
+++ src/api/cache.js
@@ -0,0 +1,19 @@
+import { InMemoryCache, makeVar } from "@apollo/client";
+
+export const isLoggedInVar = makeVar(!!localStorage.getItem('token'));
+
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        isLoggedIn: {
+          read() {
+            return isLoggedInVar();
+          }
+        }
+      }
+    }
+  }
+});
+
+export default cache;
