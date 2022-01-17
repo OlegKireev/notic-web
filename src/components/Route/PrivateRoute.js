5d71ee5bda412f381e290fbc7799608baf4a629a
Oleg Kireev <okireev@ubic.tech>
2022-01-17 10:44:58 +0300

    @/components/Route: create PrivateRoute, PublicRoute

---
+++ src/components/Route/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Redirect, Route } from "react-router-dom";
+import useAuth from "@/hooks/useAuth";
+import Preloader from "../Preloader";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isLoggedIn, loading, error } = useAuth();
+  if (loading) return <Preloader loading={loading} />
+  if (error) return <p>{error.message}</p>
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        isLoggedIn
+          ? <Component {...props}/>
+          : <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location
+                }
+              }}
+            />
+      )}
+    />
+  )
+}
+
+export default PrivateRoute;
