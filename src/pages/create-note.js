a03f71fab6510ed36def899727f03b7102b32c1d
Oleg Kireev <okireev@ubic.tech>
2022-01-18 23:50:06 +0300

    @/pages/create-note: connect with api

--- src/pages/create-note.js
+++ src/pages/create-note.js
@@ -1,14 +1,30 @@
+import { useMutation } from '@apollo/client';
 import React, { Fragment, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { CREATE_NOTE, GET_NOTE_FEED } from '../api/note';
 import NoteForm from '../components/NoteForm';
 
 const CreateNote = () => {
+  const history = useHistory();
+  const [ data, { loading, error }] = useMutation(CREATE_NOTE, {
+    // Обновляем кеш Apollo
+    refetchQueries: [{
+       query: GET_NOTE_FEED
+    }],
+    onCompleted: (data) => {
+      history.push(`note/${data.createNote.id}`);
+    }
+  });
+
   useEffect(() => {
     document.title = 'Create Note — Notedly';
   });
-
+  
   return (
     <Fragment>
-      <NoteForm />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error saving the note</p>}
+      <NoteForm action={data}/>
     </Fragment>
   );
 };
