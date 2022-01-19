48150b4196e981c72ed4e18827129ed7400ead2a
Oleg Kireev <okireev@ubic.tech>
2022-01-19 12:04:22 +0300

    @/components/Note: add NoteUser

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -3,10 +3,16 @@ import ReactMarkdown from 'react-markdown';
 import { format } from 'date-fns'
 import { Wrapper, Author, Avatar, MetaInfo } from './styled';
 import { Link } from 'react-router-dom';
+import Preloader from '../Preloader';
+import useAuth from '@/hooks/useAuth';
+import NoteUser from '../NoteUser';
 
 const Note = ({ data }) => {
+  const { isLoggedIn, loading, error } = useAuth();
   const formatedDate = format(data.createdAt, 'MMM Do YYYY');
 
+  if (loading) return <Preloader loading={loading}/>;
+  if (error) return <p>{error.message}</p>;
   return (
     <Wrapper>
       <Author>
@@ -19,6 +25,9 @@ const Note = ({ data }) => {
         <time dateTime={data.createdAt}>{formatedDate}</time>
       </MetaInfo>
       <Link to={`/note/${data.id}`}>Permalink</Link>
+      {isLoggedIn && (
+        <NoteUser note={data} />
+      )}
     </Wrapper>
   )
 };
