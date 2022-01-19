205e9f88a29d2476126a4daf34920472edc05af2
Oleg Kireev <okireev@ubic.tech>
2022-01-19 10:42:51 +0300

    @/pages/favorites: connect wih api

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,13 +1,18 @@
-import React, { Fragment, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import React, { useEffect } from 'react';
+import { GET_MY_FAVORITES } from '../api/note';
+import NoteList from '../components/NoteList';
+import Preloader from '../components/Preloader';
 
 const Favorites = () => {
+  const { data, loading, error } = useQuery(GET_MY_FAVORITES);
   useEffect(() => {
     document.title = 'Favorites — Notedly';
   });
+  if (loading) return <Preloader loading={loading}/>
+  if (error) return <p>{error.message}</p>
   return (
-    <Fragment>
-      <p>These are my favorites</p>
-    </Fragment>
+    <NoteList data={data.me.favoriteNotes} />
   );
 };
 
