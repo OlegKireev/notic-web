5c790152af68e3d908837e72d576e3950407efcd
Oleg Kireev <okireev@ubic.tech>
2022-01-19 10:41:18 +0300

    @/api/note: add GET_MY_NOTES, GET_MY_FAVORITES

--- src/api/note.js
+++ src/api/note.js
@@ -20,6 +20,46 @@ export const GET_NOTE_FEED = gql`
 }
 `;
 
+export const GET_MY_NOTES = gql`
+query me {
+  me { 
+    id
+    username
+    notes {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  } 
+}
+`;
+
+export const GET_MY_FAVORITES = gql`
+query me {
+  me { 
+    id
+    username
+    favoriteNotes {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  }
+}
+`;
+
 export const GET_NOTE = gql`
   query note($id: ID!) {
     note(id: $id) {
