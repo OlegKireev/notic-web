21e828369a46a27de98fe1ddace9d8e7d0ea4be2
Oleg Kireev <okireev@ubic.tech>
2022-01-19 13:32:10 +0300

    @/components/NoteUser: add FavoriteNote

--- src/components/NoteUser/index.js
+++ src/components/NoteUser/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import Preloader from '../Preloader';
 import { GET_ME } from '@/api/user';
 import DeleteNote from '../DeleteNote';
+import FavoriteNote from '../FavoriteNote';
 
 const NoteUser = ({ note }) => {
   const { data, loading, error } = useQuery(GET_ME);
 
   if (loading) return <Preloader loading={loading} />;
   if (error) return <p>{error.message}</p>;
-  return data.me.id === note.author.id && (
+  return (
     <div>
-      <Link to={`/edit-note/${note.id}`}>Edit</Link>
-      <DeleteNote id={note.id} />
+      <FavoriteNote
+        me={data.me}
+        noteId={note.id}
+        favoriteCount={note.favoriteCount}
+      />
+      {data.me.id === note.author.id && (
+        <Fragment>
+          <Link to={`/edit-note/${note.id}`}>Edit</Link>
+          <DeleteNote id={note.id} />
+        </Fragment>
+      )}
     </div>
   )
 };
