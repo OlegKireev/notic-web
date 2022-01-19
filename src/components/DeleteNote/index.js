648bd23407ad14893a54983cb165cf7c5b268373
Oleg Kireev <okireev@ubic.tech>
2022-01-19 12:43:58 +0300

    @/components/DeleteNote: create

---
+++ src/components/DeleteNote/index.js
@@ -0,0 +1,27 @@
+import { useMutation } from '@apollo/client';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { DELETE_NOTE, GET_NOTES, GET_MY_NOTES } from '../../api/note';
+import Button from '../Button';
+
+const DeleteNote = ({ id }) => {
+  const history = useHistory();
+
+  const [deleteNote] = useMutation(DELETE_NOTE, {
+    variables: {
+      id
+    },
+    refetchQueries: [{ query: GET_MY_NOTES, GET_NOTES }],
+    onCompleted: () => {
+      // Перенаправляем пользователя на страницу "my notes"
+      history.push('/my-notes');
+    }
+  });
+
+  console.log(deleteNote);
+  
+
+  return <Button onClick={deleteNote}>Delete Note</Button>;
+};
+
+export default DeleteNote;
