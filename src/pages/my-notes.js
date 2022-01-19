46e906ba96a9f579cf5b6236eaa929a6635f83cd
Oleg Kireev <okireev@ubic.tech>
2022-01-19 10:42:31 +0300

    @/pages/my-notes: connect with api

--- src/pages/my-notes.js
+++ src/pages/my-notes.js
@@ -1,13 +1,19 @@
-import React, { Fragment, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import React, { useEffect } from 'react';
+import { GET_MY_NOTES } from '@/api/note';
+import NoteList from '../components/NoteList';
+import Preloader from '../components/Preloader';
 
 const MyNotes = () => {
+  const { data, loading, error } = useQuery(GET_MY_NOTES)
+
   useEffect(() => {
     document.title = 'My Notes — Notedly';
   });
+  if (loading) return <Preloader loading={loading}/>;
+  if (error) return `Error! ${error.message}`;
   return (
-    <Fragment>
-      <p>These are my notes</p>
-    </Fragment>
+    <NoteList data={data.me.notes} />
   );
 };
 
