521528789e14a88551633f27f46720d2eb26fd3b
Oleg Kireev <okireev@ubic.tech>
2022-01-19 12:03:10 +0300

    @/components/NoteUser: render if it is a current user

--- src/components/NoteUser/index.js
+++ src/components/NoteUser/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
+import Preloader from '../Preloader';
+import { GET_ME } from '@/api/user';
 
 const NoteUser = ({ note }) => {
-  return <Link to={`/edit/${note.id}`}>Edit</Link>;
+  const { data, loading, error } = useQuery(GET_ME);
+
+  if (loading) return <Preloader loading={loading} />;
+  if (error) return <p>{error.message}</p>;
+  return data.me.id === note.author.id && (
+    <Link to={`/edit-note/${note.id}`}>Edit</Link>
+  )
 };
 
 export default NoteUser;
