5318cc36d2f93824be5b60c9473edb2f202e4693
Oleg Kireev <okireev@ubic.tech>
2022-01-19 12:27:46 +0300

    @/hooks/useAuth: reset clent cache after logout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,11 @@
-import { useQuery } from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 import { IS_LOGGED_IN } from '../api/auth';
 import { isLoggedInVar } from '../api/cache';
 
 const useAuth = () => {
   const history = useHistory();
+  const client = useApolloClient();
 
   const { data, loading, error } = useQuery(IS_LOGGED_IN);
   const { isLoggedIn } = data; 
@@ -13,6 +14,7 @@ const useAuth = () => {
     // Удаляем токен
     localStorage.removeItem('token');
     isLoggedInVar(false);
+    client.resetStore();
     // Перенаправляем пользователя на домашнюю страницу
     history.push('/');
   };
