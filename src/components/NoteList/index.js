4f9bb91a8bef534fe8e7884b26538768d2849230
Oleg Kireev <okireev@ubic.tech>
2022-01-22 11:25:34 +0300

    @/components/NoteList add MasonryLayout

--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import Preloader from '../Preloader';
-import Note from '../Note';
-import { Wrapper, List, LoadMoreButton } from './styled';
+import { Wrapper, Note, LoadMoreButton } from './styled';
+import MasonryLayout from '../MasonryLayout';
+import { useHistory } from 'react-router-dom';
 
-const NoteList = ({ data, loading, hasMore, onLoadMoreClick }) => (
-  <Preloader loading={loading}>
-    <Wrapper>
-      {!data.length
-        ? (
-          <p>No notes yet</p>
-        )
-        : (
-          <List>
-            {data.map((note) => (
-              <Note data={note} key={note.id} />
-            ))}
-          </List>
-        )
-      }
-      {hasMore && <LoadMoreButton onClick={() => onLoadMoreClick()}>Load more...</LoadMoreButton>}
-    </Wrapper>
-  </Preloader>
-)
+const NoteList = ({ data, loading, hasMore, onLoadMoreClick }) => {
+  const history = useHistory();
+
+  return (
+    <Preloader loading={loading}>
+      <Wrapper>
+        {!data.length
+          ? (
+            <p>No notes yet</p>
+          )
+          : (
+            <MasonryLayout>
+              {data.map((note) => (
+                <Note 
+                  key={note.id}
+                  data={note}
+                  tabIndex="0"
+                  role="link"
+                  onClick={() => {
+                    history.push(`/note/${note.id}`);
+                  }}
+                />
+              ))}
+            </MasonryLayout>
+          )
+        }
+        {hasMore && <LoadMoreButton onClick={() => onLoadMoreClick()}>Load more...</LoadMoreButton>}
+      </Wrapper>
+    </Preloader>
+  )
+}
 
 export default NoteList;
