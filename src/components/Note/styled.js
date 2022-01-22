e3c5c84dffdf3de80fb44fb5b0ac6e43c1c922bf
Oleg Kireev <okireev@ubic.tech>
2022-01-22 11:26:53 +0300

    @/components/Note/styled: update styles

--- src/components/Note/styled.js
+++ src/components/Note/styled.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.article`
-  max-width: 800px;
-  margin: 0 auto;
+  background-color: var(--color-white);
+  padding: 1em;
+  font-size: .875rem;
+  border-radius: .5em;
+
+  & code {
+    display: block;
+    overflow-x: scroll;
+  }
 `
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const Author = styled.div`
   display: flex;
@@ -14,7 +26,13 @@ export const Avatar = styled.img`
   max-height: 1.5rem;
   margin-right: .5em;
   object-fit: cover;
+  border-radius: 50%;
 `
+
+export const CreatedTime = styled.time`
+  font-size: .75em;
+  color: var(--color-gray-500);
+`;
 
 export const MetaInfo = styled.div`
   display: flex;
