f73125c5e76719405e4025ac38e0b57d1cfbeb8c
Oleg Kireev <okireev@ubic.tech>
2022-01-22 11:27:53 +0300

    @/components/Note: update layout

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { format } from 'date-fns'
-import { Wrapper, Author, Avatar, MetaInfo } from './styled';
+import { Wrapper, Header, Author, Avatar, CreatedTime, MetaInfo } from './styled';
 import { Link } from 'react-router-dom';
 import Preloader from '../Preloader';
 import useAuth from '@/hooks/useAuth';
 import NoteUser from '../NoteUser';
 
-const Note = ({ data }) => {
+const Note = ({ data, ...props }) => {;
   const { isLoggedIn, loading, error } = useAuth();
   const formatedDate = format(data.createdAt, 'MMM Do YYYY');
 
   if (loading) return <Preloader loading={loading}/>;
   if (error) return <p>{error.message}</p>;
   return (
-    <Wrapper>
-      <Author>
-        <Avatar src={data.author.avatar} alt={`${data.author.username} avatar`} height={25} />
-        <span>{data.author.username}</span>{' '}
-      </Author>
+    <Wrapper {...props}>
+      <Header>
+        <Author>
+          <Avatar src={data.author.avatar} alt={`${data.author.username} avatar`} height={25} />
+          <span>{data.author.username}</span>{' '}
+        </Author>
+        <CreatedTime dateTime={data.createdAt}>
+          {formatedDate}
+        </CreatedTime>
+      </Header>
       <ReactMarkdown>{data.content}</ReactMarkdown>
       <MetaInfo>
         <span>Favorites: {data.favoriteCount}</span>
-        <time dateTime={data.createdAt}>{formatedDate}</time>
       </MetaInfo>
-      <Link to={`/note/${data.id}`}>Permalink</Link>
       {isLoggedIn && (
         <NoteUser note={data} />
       )}
