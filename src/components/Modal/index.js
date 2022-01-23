d05bd076d2e8400dfc94b814ae5b8d483934346a
Oleg Kireev <okireev@ubic.tech>
2022-01-23 18:15:31 +0300

    @/components/Modal: change close button kind

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../Button';
 import { Backdrop, Content, Dialog, Footer, Header } from './styled';
+import IconClose from '@/Icons/Close';
 
 const Modal = ({
   children,
@@ -13,9 +14,10 @@ const Modal = ({
     <Backdrop>
       <Dialog>
         <Header>
-          <Button kind="close"
+          <Button kind="ghost"
             onClick={onClose}
           >
+            <IconClose />
           </Button>
         </Header>
         <Content>
