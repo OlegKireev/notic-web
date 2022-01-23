d6c684128c019a7b12a329e52242c7d00a3b9ebc
Oleg Kireev <okireev@ubic.tech>
2022-01-23 18:15:02 +0300

    @/theme/keyframes: add slideDown

--- src/theme/keyframes.js
+++ src/theme/keyframes.js
@@ -7,4 +7,13 @@ export const fadeIn = keyframes`
   100% {
     opacity: 1;
   }
-`
+`;
+
+export const slideDown = keyframes`
+  0% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
