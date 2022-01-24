c25ea74812b6a222f2f70dba2fbbcd10632ec20a
Oleg Kireev <okireev@ubic.tech>
2022-01-24 10:35:22 +0300

    @/components/Navigarion: hide links to unauthrized users

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import pathnames from '@/constants/pathnames';
 import { NavList } from './styled';
+import useAuth from '@/hooks/useAuth';
 
-const Navigation = () => (
-  <nav>
-    <NavList>
-      {pathnames.map(({ path, label, exact }) => (
-        <li key={path}>
-          <NavLink to={path} exact={exact}>{label}</NavLink>
-        </li>
-      ))}
-    </NavList>
-  </nav>
-)
+const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+  const links = !isLoggedIn 
+    ? pathnames.filter((pathname) => pathname.private === false)
+    : pathnames;
+
+  return (
+    <nav>
+      <NavList>
+        {links.map(({ path, label, exact }) => (
+          <li key={path}>
+            <NavLink to={path} exact={exact}>{label}</NavLink>
+          </li>
+        ))}
+      </NavList>
+    </nav>
+  )
+}
 
 export default Navigation;
--- src/constants/pathnames.js
+++ src/constants/pathnames.js
@@ -3,18 +3,22 @@ export const pathnames = [
     path: '/',
     label: 'Home',
     exact: true,
+    private: false,
   },
   {
     path: '/my-notes',
     label: 'My notes',
+    private: true,
   },
   {
     path: '/favorites',
     label: 'Favotites',
+    private: true,
   },
   {
     path: '/create-note',
     label: 'Create Note',
+    private: true,
   },
 ];
 
